bb253fe29e8b9da0031e061928f69f6b4dd2cb6d
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2023-07-15 22:30:43 -0700

    Add first version of all composite factions

--- faction_palettes/bursty.lua
+++ faction_palettes/bursty.lua
@@ -1,53 +1,50 @@
-
--- Bursty's faction number is 41
-
--- terran core
-{41000,     extends = 800,      group = 41}
-
--- Bee hull
-{41002,     extends = 12004,    group = 41}
-{41003,     extends = 12006,    group = 41}
-{41004,     extends = 12008,    group = 41}
-
-{41005,     extends = 12012,    group = 41}
-{41006,     extends = 12014,    group = 41}
-{41007,     extends = 12016,    group = 41}
-
-{41008,     extends = 12020,    group = 41}
-{41009,     extends = 12022,    group = 41}
-{41010,     extends = 12024,    group = 41}
-
-{41011,     extends = 12052,    group = 41}
-{41012,     extends = 12054,    group = 41}
-{41013,     extends = 12056,    group = 41}
-
-{41014,     extends = 12028,    group = 41}
-{41015,     extends = 12030,    group = 41}
-{41016,     extends = 12032,    group = 41}
-
-{41017,     extends = 12044,    group = 41}
-{41018,     extends = 12046,    group = 41}
-{41019,     extends = 12048,    group = 41}
-
--- terran hull
-{41030,     extends = 802,      group = 41}
-{41032,     extends = 852,      group = 41}
-
-{41034,     extends = 857,      group = 41}
-{41036,     extends = 846,      group = 41}
-
-{41038,     extends = 826,      group = 41}
-{41040,     extends = 876,      group = 41}
-
-{41042,     extends = 827,      group = 41}
-{41044,     extends = 877,      group = 41}
-
-{41046,     extends = 828,      group = 41}
-{41048,     extends = 878,      group = 41}
-
-{41050,     extends = 829,      group = 41}
-{41052,     extends = 879,      group = 41}
-
--- Thrusters --
--- terran
-{41060,     extends = 803,      group = 41}
+{50000, extends = 800, group = 50}
+-- Bee Hull
+{50002, extends = 12004, group = 50}
+{50004, extends = 12006, group = 50}
+{50006, extends = 12008, group = 50}
+{50008, extends = 12020, group = 50}
+{50010, extends = 12022, group = 50}
+{50012, extends = 12024, group = 50}
+{50014, extends = 12012, group = 50}
+{50016, extends = 12014, group = 50}
+{50018, extends = 12016, group = 50}
+{50020, extends = 12028, group = 50}
+{50022, extends = 12030, group = 50}
+{50024, extends = 12032, group = 50}
+{50026, extends = 12052, group = 50}
+{50028, extends = 12054, group = 50}
+{50030, extends = 12056, group = 50}
+{50032, extends = 12044, group = 50}
+{50034, extends = 12046, group = 50}
+{50036, extends = 12048, group = 50}
+-- Terran hull
+{50038, extends = 802, group = 50}
+{50040, extends = 852, group = 50}
+{50042, extends = 857, group = 50}
+{50044, extends = 846, group = 50}
+{50046, extends = 826, group = 50}
+{50048, extends = 876, group = 50}
+{50050, extends = 827, group = 50}
+{50052, extends = 877, group = 50}
+{50054, extends = 828, group = 50}
+{50056, extends = 878, group = 50}
+{50058, extends = 829, group = 50}
+{50060, extends = 879, group = 50}
+-- Weapons
+{50062, extends = 15005, group = 50}
+{50064, extends = 845, group = 50}
+{50066, extends = 15001, group = 50, points = 288}
+{50068, extends = 814, group = 50}
+{50070, extends = 314, group = 50}
+-- shields
+{50072, extends = 850, group = 50}
+-- generator
+{50074, extends = 1508, group = 50}
+-- thrust
+{50076, extends = 403, group = 50}
+{50078, extends = 404, group = 50}
+{50080, extends = 405, group = 50}
+{50082, extends = 803, group = 50}
+{50084, extends = 804, group = 50}
+{50086, extends = 805, group = 50}
---
+++ faction_palettes/generalist.lua
@@ -0,0 +1,41 @@
+-- Core
+{52000, extends = 15000, group = 52}
+-- generators
+{52002, extends = 801, group = 52}
+{52004, extends = 834, group = 52}
+-- hull
+{52006, extends = 826, group = 52}
+{52008, extends = 802, group = 52}
+{52010, extends = 828, group = 52}
+{52012, extends = 827, group = 52}
+{52014, extends = 857, group = 52}
+{52016, extends = 829, group = 52}
+{52018, extends = 876, group = 52}
+{52020, extends = 852, group = 52}
+{52022, extends = 878, group = 52}
+{52024, extends = 877, group = 52}
+{52026, extends = 846, group = 52}
+{52028, extends = 879, group = 52}
+{52030, extends = 886, group = 52}
+{52032, extends = 862, group = 52}
+{52034, extends = 888, group = 52}
+{52036, extends = 887, group = 52}
+{52038, extends = 859, group = 52}
+{52040, extends = 889, group = 52}
+{52042, extends = 860, group = 52}
+{52044, extends = 861, group = 52}
+-- thrusters
+{52046, extends = 803, group = 52}
+{52048, extends = 804, group = 52}
+{52050, extends = 805, group = 52}
+-- Weapon Time!
+{52052, extends = 251, group = 52}
+{52054, extends = 252, group = 52}
+{52056, extends = 253, group = 52}
+{52058, extends = 310, group = 52}
+{52060, extends = 831, group = 52}
+{52062, extends = 881, group = 52}
+{52064, extends = 891, group = 52}
+{52066, extends = 848, group = 52}
+-- shield
+{52068, extends = 850, group = 52}
