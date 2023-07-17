7ba231d3be887bd9b23d45800ae5f01e8f813f2b
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2023-07-17 13:17:28 -0700

    add new ships and factions.lua

---
+++ factions.lua
@@ -0,0 +1,42 @@
+{
+  50 = {
+    name="Bursty",
+    playable=2,
+    primaries=3,
+    color0=0xF8BC04, -- bee yellow
+    color1=0xaaaaaa, -- terran grey
+    color2=0x113077, -- terran blue
+    aiflags=SMART_FIRE|WANDER|SOCIAL|DODGES|FLOCKING|AGGRESSIVE,
+    start="50_starter"
+  },
+  51 = {
+    name="Big Bang",
+    playable=2,
+    primaries=3,
+    color0=0x202859, -- terran blue / tink blue mix
+    color1=0x79a82d, -- farmer green
+    color2=0xe1a71c, -- red red
+    aiflags=SMART_FIRE|WANDER|SOCIAL|DODGES|FLOCKING|CAUTIOUS,
+    start="51_starter"
+  },
+  52 = {
+    name="Broadbreath",
+    playable=2,
+    primaries=3,
+    color0=0x113077, -- terran blue
+    color1=0xaaaaaa, -- terran grey
+    color2=0x79a82d, -- farmer green
+    aiflags=SMART_FIRE|WANDER|SOCIAL|DODGES|FLOCKING,
+    start="52_starter"
+  },
+  55 = {
+    name="Brad's Brawlers",
+    playable=2,
+    primaries=3,
+    color0=0x074480, -- crystal blue
+    color1=0xaaaaaa, -- terran grey
+    color2=0x79a82d, -- farmer green
+    aiflags=SMART_FIRE|WANDER|SOCIAL|DODGES|AGGRESSIVE,
+    start="55_starter"
+  },
+}
